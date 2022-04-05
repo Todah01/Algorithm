65784d582f22f26ea5d009a704ac2464ddc96cf9
Tony <wobo5695@gmail.com>
2022-04-05 14:50:04 +0900

    초기 커밋.

---
+++ Algorithm/1_algorithm.c
@@ -0,0 +1,28 @@
+#if 0
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 1, b = 3, c = 2;
+	if ((b >= a && c <= a) || (b <= a && c >= a))
+	{
+		printf(a);
+	}
+	else if ((a > b && c < b) || (a<b && c>b))
+	{
+		printf(b);
+	}
+	printf(c);
+}
+#endif
+
+#if 1
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World");
+}
+#endif
+
+
