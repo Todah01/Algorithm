f3da55dd5826b233bdb1a4c362684a62a87a46d5
Tony <wobo5695@gmail.com>
2022-04-19 16:27:23 +0900

    2022.04.19_recursion

--- Algorithm/5_recursion.c
+++ Algorithm/5_recursion.c
@@ -1,5 +1,5 @@
 //1¹ø
-#if 1
+#if 0
 #include <stdio.h>
 #pragma warning(disable : 4996)
 
@@ -15,5 +15,43 @@ int main()
 	int n;
 	scanf("%d", &n);
 	printf("%d\n", fibonacci(n));
+}
+#endif
+
+//2¹ø
+#if 1
+#include <stdio.h>
+#pragma warning(disable : 4996)
+
+void star(int i, int j, int n)
+{
+	if ((i / n) % 3 == 1 && (j / n) % 3 == 1)
+	{
+		printf(" ");
+	}
+	else if (n / 3 == 0)
+	{
+		printf("*");
+	}
+	else
+	{
+		star(i, j, n / 3);
+	}
+}
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			star(i, j, n);
+		}
+		printf("\n");
+	}
 }
 #endif
