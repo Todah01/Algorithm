03c6d547b87122999e3ada776f561d9a152f9022
Tony <wobo5695@gmail.com>
2022-04-24 20:27:23 +0900

    2022.04.24_set

--- Algorithm/7_set.c
+++ Algorithm/7_set.c
@@ -1,4 +1,4 @@
-#if 0
+#if 1
 #include <stdio.h>
 #include <stdlib.h>
 #include "7_set_header.h"
@@ -219,6 +219,51 @@ void Terminate(IntSet *s)
 	if (s->set != NULL) {
 		free(s->set);							
 		s->max = s->num = 0;
+	}
+}
+#endif
+
+#if 1
+#include <stdio.h>
+#include "7_set_header.h"
+#pragma warning(disable : 4996)
+
+enum {ADD, RMV, SCH};
+
+int scan_data(int sw)
+{
+	int data;
+	switch (sw)
+	{
+	case ADD: printf("추가할 데이터 : "); break;
+	case RMV: printf("삭제할 데이터 : "); break;
+	case SCH: printf("검색할 데이터 : "); break;
+	}
+	scanf("%d", &data);
+
+	return data;
+}
+
+int main()
+{
+	IntSet s1, s2, temp;
+	Initialize(&s1, 512); Initialize(&s2, 512); Initialize(&temp, 512);
+
+	while (1)
+	{
+		int m, x, idx;
+		PrintLn(&s1); PrintLn(&s2);
+
+		scanf("%d", &m);
+
+		if (m == 0) break;
+		switch (m)
+		{
+			case 1: x = scan_data(ADD); Add(&s1, x); break;
+			case 2: x = scan_data(RMV); Remove(&s1, x); break;
+			case 3: x = scan_data(SCH); idx = IsMember(&s1, x);
+				printf("s1에 들어있%s.\n", idx >= 0 ? "습니다" : "지 않습니다"); break;
+		}
 	}
 }
 #endif
--- Algorithm/7_set_header.h
+++ Algorithm/7_set_header.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef ___IntSet
+#define ___IntSet
+
+typedef struct
+{
+	int max;
+	int num;
+	int* set; // 집합 본체의 배열(의 첫 번째 요소에 대한 포인터)
+} IntSet;
+
+int Initialize(IntSet* s, int max);
+
+int IsMember(const IntSet* s, int n);
+
+void Add(IntSet* s, int n);
+
+void Remove(IntSet* s, int n);
+
+int Size(const IntSet* s);
+
+void Assign(IntSet* s1, const IntSet* s2);
+
+int Equal(const IntSet* s1, const IntSet* s2);
+#endif
