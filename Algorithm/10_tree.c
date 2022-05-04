33211c1df763f944bbbdc25a0a0a57d134defa53
Tony <wobo5695@gmail.com>
2022-05-04 19:48:08 +0900

    2022.05.04_tree

--- Algorithm/10_tree.c
+++ Algorithm/10_tree.c
@@ -1,7 +1,7 @@
 #if 1
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "11_member.h"
 #include "10_tree.h"
 
 static BinNode* AllocBinNode(void)
@@ -20,10 +20,33 @@ BinNode* Search(BinNode* p, const Member* x)
 {
 	int cond;
 	if (p == NULL) return NULL;
+	else if ((cond = MemberNoCmp(x, &p->data)) == 0) return p;
 	else if (cond < 0) Search(p->left, x);
 	else Search(p->right, x);
 }
 
+BinNode* Add(BinNode* p, const Member* x)
+{
+	int cond;
+	if (p == NULL)
+	{
+		p = AllocBinNode();
+		SetBinNode(p, x, NULL, NULL);
+	}
+	else if ((cond = MemberNoCmp(x, &p->data)) == 0) printf("오류발생 %d는 이미 등록되어 있습니다\n", x->no);
+	else if (cond < 0) p->left = Add(p->left, x);
+	else p->right = Add(p->right, x);
+}
+
+void PrintTree(const BinNode* p)
+{
+	if (p != NULL)
+	{
+		PrintTree(p->left);
+		PrintLnMember(&p->data);
+		PrintTree(p->right);
+	}
+}
 /* 1. p에 루트 포인터를 대입합니다 (루트를 선택)
    2. 삽입할 키 key와 p의 키 값을 비교합니다. 값이 같다면 삽입에 실패합니다(종료)
    -> 값이 같지 않은 경우 key 값이 삽입할 값보다 작으면 : 왼쪽 자식노드가 없는 경우 노드 삽입, 왼쪽 자식 노드가 있는 경우 선택한 노드 왼쪽 자식 노드 포인터 대입
@@ -31,7 +54,7 @@ BinNode* Search(BinNode* p, const Member* x)
 #endif
 
 //Q1
-#if 0
+#if 1
 void PrintTreeReverse(const BinNode *p)
 {
 	if (p != NULL) {
@@ -68,3 +91,145 @@ BinNode *GetMaxNode(const BinNode *p)
 }
 #endif
 
+//1번
+#if 0
+#include <stdio.h>
+int arr[26][2];
+
+void pre(int x) {
+	if (x < 0) return;
+
+	printf("%c", x + 65);
+	pre(arr[x][0]);
+	pre(arr[x][1]);
+
+	return;
+}
+
+void in(int x) {
+	if (x < 0) return;
+
+	in(arr[x][0]);
+	printf("%c", x + 65);
+	in(arr[x][1]);
+
+	return;
+}
+
+void post(int x) {
+	if (x < 0) return;
+
+	post(arr[x][0]);
+	post(arr[x][1]);
+	printf("%c", x + 65);
+
+	return;
+}
+
+int main() {
+	int n;
+	char a, b, c;
+
+	scanf("%d", &n);
+
+	for (int i = 0;i < n;i++) {
+		scanf("\n%c \n%c \n%c", &a, &b, &c);
+
+		arr[a - 65][0] = b - 65;
+		arr[a - 65][1] = c - 65;
+	}
+
+	pre(0); printf("\n");
+	in(0); printf("\n");
+	post(0); printf("\n");
+
+	return 0;
+}
+#endif
+
+//1번__
+#if 0
+#include <stdio.h>
+#include "10_tree.h"
+#include "11_member.h"
+#pragma warning(disable : 4996)
+
+int main()
+{
+	int n;
+	char x, y, z;
+	BinNode* temp = NULL;
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%c %c %c", &x, &y, &z);
+		Add(temp, &x);
+	}
+}
+#endif
+
+//2번
+#if 1
+
+#endif
+
+//3번
+#if 0
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int n, u, v, cost;
+int node, ans;
+bool visit[100001];
+vector<pair<int, int>> graph[100001];
+
+void DFS(int x, int dist)
+{
+	visit[x] = true;
+
+	if (dist > ans)
+	{
+		ans = dist;
+		node = x;
+	}
+
+	for (int i = 0; i < graph[x].size(); i++)
+	{
+		int next_node = graph[x][i].first;
+		int next_dist = graph[x][i].second + dist;
+		if (!visit[next_node])
+		{
+			visit[next_node] = true;
+			DFS(next_node, next_dist);
+			visit[next_node] = false;
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+
+	for (int i = 1; i < n; i++)
+	{
+		cin >> u >> v >> cost;
+		graph[u].push_back({ v,cost });
+		graph[v].push_back({ u,cost });
+	}
+
+	DFS(1, 0);
+	ans = 0;
+	for (int i = 1; i <= n; i++)
+		visit[i] = false;
+	DFS(node, 0);
+	cout << ans << '\n';
+}
+#endif
--- Algorithm/11_member.h
+++ Algorithm/11_member.h
@@ -1,11 +1,28 @@
-#ifndef __Member_Sec
-#define __Member_Sec
+/* 회원 데이터 Member(헤더) */
+#ifndef ___Member
+#define ___Member
 
+/*--- 회원 데이터 ---*/
 typedef struct {
-	int no;
-	char name[20];
+	int no; 				/* 번호 */
+	char name[20]; 		    /* 이름 */
 } Member;
 
-#define MEMBER_NO 1
-#define MEMBER_NAME 2
+#define MEMBER_NO		1 	/* 번호를 나타내는 정수 값 */
+#define MEMBER_NAME		2 	/* 이름을 나타내는 상수 값 */
+
+/*--- 회원 번호 비교 함수 ---*/
+int MemberNoCmp(const Member* x, const Member* y);
+
+/*--- 회원 이름 비교 함수 ---*/
+int MemberNameCmp(const Member* x, const Member* y);
+
+/*--- 회원 데이터를 표시(줄 바꿈 없음) ---*/
+void PrintMember(const Member* x);
+
+/*--- 회원 데이터를 표시(줄 바꿈 있음) ---*/
+void PrintLnMember(const Member* x);
+
+/*--- 회원 데이터를 읽어 들이기 ---*/
+Member ScanMember(const char* message, int sw);
 #endif
